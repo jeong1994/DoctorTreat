d62642383b7fc154456a47f7d70777879a892fd0
nowwwjs <jaeseo1227@gmail.com>
2024-08-28 21:28:00 +0900

    240828 21:27 이재서 memberjoin js 완료

--- workspace/js/myPage/member/memberjoin.js
+++ workspace/js/myPage/member/memberjoin.js
@@ -6,7 +6,6 @@ const messages = [
   "비밀번호가 필요합니다",
   "비밀번호확인이 필요합니다",
   "이름이 필요합니다",
-  "주소가 필요합니다",
   "생년월일이 필요합니다",
   "전화번호가 필요합니다",
 ];
@@ -69,7 +68,7 @@ focusBtn.forEach((inputGroup, index) => {
         }
 
          // 7) 생년월일 : 생년월일은 8자리 숫자로 입력해주세요
-         if (index === 5) { 
+         if (index === 7) { 
           const birthdatePattern = /^\d{8}$/;
           if (!birthdatePattern.test(input.value)) {
             input.style.border = "2px solid red"; 
@@ -153,3 +152,36 @@ document.querySelector(".signup-btn").addEventListener("click", function (event)
       window.location.href = "memberJoinfinish.html"; // 이동
   }
 });
+
+// 우편번호 api
+function execDaumPostcode() {
+  new daum.Postcode({
+      oncomplete: function(data) {
+          var extraAddr = '';
+
+          if (data.userSelectedType === 'R') {
+              addr = data.roadAddress;
+          } else {
+              addr = data.jibunAddress;
+          }
+
+          if (data.userSelectedType === 'R') {
+
+              if (data.bname !== '' && /[동|로|가]$/g.test(data.bname)) {
+                  extraAddr += data.bname;
+              }
+              if (data.buildingName !== '' && data.apartment === 'Y') {
+                  extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+              }
+              if (extraAddr !== '') {
+                  extraAddr = ' (' + extraAddr + ')';
+              }
+
+              addr += extraAddr;
+          }
+          document.getElementById('postcode').value = data.zonecode;
+          document.getElementById("address").value = addr;
+          document.getElementById("detailAddress").focus();
+      }
+  }).open();
+}
